8dd7a5991a6a1036f6ef28ac113439121be86e7c
Joseph Macam <jdmacam@gmail.com>
2020-02-12 09:40:12 -0800

    Practice 2/12

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 using std::cin;
 using std::cout;
-
+using std::endl;
 int main()
 {
-    cout<<"Hi, please enter two numbers: ";
+    cout<< "Enter age and birth year: ";
     int num1, num2;
     cin >> num1;
     cin >> num2;
+    cout << "You are " << num1 << " years old and " << num2 << "er." <<endl;
     return 0;
 }
